25a38e82b6f3cea9e3429fa22ca0f4b512fc34aa
Usuario <Usuario@CPD03-M18>
2024-04-15 12:22:06 -0300

    Adicionar arquivos de projeto.

---
+++ AppViagem/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace AppViagem
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+            NavigationPage.SetBackButtonTitle(this, null);
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+}
---
+++ AppViagem/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AppViagem
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Rubik-VariableFont_wght.ttf", "Rubik");
+                });
+
+
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+
+
+        }
+    }
+}
